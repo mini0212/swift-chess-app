1d65121c5f9cd50cf28bd73a9e3c46b960ec698f
Seongmin Kim <ksmini0212@gmail.com>
2022-09-28 11:56:21 +0900

    클래스별 파일 나누기

---
+++ ChessApp/ChessApp/Chess/Position.swift
@@ -0,0 +1,28 @@
+//
+//  Position.swift
+//  ChessApp
+//
+//  Created by seongmin.kim on 2022/09/28.
+//
+
+import Foundation
+
+enum File: Int, CaseIterable {
+    case A = 0
+    case B,C,D,E,F,G,H
+}
+
+enum Rank: Int, CaseIterable {
+    case One = 0
+    case Two, Three, Four, Five, Six, Seven, Eight
+}
+
+final class Position {
+    let file: File
+    let rank: Rank
+
+    init(file: File, rank: Rank) {
+        self.rank = rank
+        self.file = file
+    }
+}
