e4a7187a858b6634532f4d4597617ad7e511befc
Seongmin Kim <ksmini0212@gmail.com>
2022-10-03 21:54:02 +0900

    기존 board 수정.

--- ChessApp/ChessApp/Chess/Board.swift
+++ ChessApp/ChessApp/Chess/Board.swift
@@ -10,57 +10,61 @@ import Foundation
 final class Board {
     private struct Constants {
         static let maxPawnCount: Int = 8 // pawn의 최대 갯수
-        static let blackPawnInitPosition: Int = Position.Rank.Two.rawValue // 검은말 초기 위치
-        static let whitePawnInitPosition: Int = Position.Rank.Seven.rawValue // 흰말 초기 위치
+//        static let blackPawnInitPosition: Int = Position.Rank.Two.rawValue // 검은말 초기 위치
+//        static let whitePawnInitPosition: Int = Position.Rank.Seven.rawValue // 흰말 초기 위치
     }
     
-    private var board: [[Pawn?]] = []
+    private var board: [[Piece?]] = []
     
-    // 체스판을 생성한다
     init() {
-        board = [[Pawn?]](repeating: [Pawn?](repeating: nil, count: Position.File.allCases.count), count: Position.Rank.allCases.count)
+        board = [[Piece?]](repeating: [Piece?](repeating: nil, count: Position.File.allCases.count), count: Position.Rank.allCases.count)
     }
     
-    // 체스보드 초기화 및 체스 말 생성
     func startGame() {
-        print("체스 보드를 초기화 했습니다.\n")
-        for i in (0..<Position.File.allCases.count) {
-            guard i < Constants.maxPawnCount else { return }
-            board[Constants.blackPawnInitPosition][i] = Pawn(color: .black)
-            board[Constants.whitePawnInitPosition][i] = Pawn(color: .white)
-        }
+        initBoard(with: Pawn(color: .black), position: Pawn(color: .black).startPosition())
+        initBoard(with: Pawn(color: .white), position: Pawn(color: .white).startPosition())
+        
+        print(displayBoard())
+
     }
     
     func move(pawn: Pawn, from: Position, to: Position) {
-        board[from.rank.rawValue][from.file.rawValue] = nil
-        board[to.rank.rawValue][to.file.rawValue] = pawn
+//        guard checkEnableToMove(pawn: pawn, from: from, to: to) else { return }
+//        board[from.rank.rawValue][from.file.rawValue] = nil
+//        board[to.rank.rawValue][to.file.rawValue] = pawn
     }
     
     // 움직일 pawn의 현재 위치와 목표 위치를 받아온다
     // 현재 위치와 목표 위치가 각 pawn의 움직일 수 있는 조건에 맞는지 확인한다
-    func checkEnableToMove(pawn: Pawn, from: Position, to: Position) -> Bool {
-        return pawnAvailableCurrent(of: pawn, position: from)
-        && pawn.checkEnableToMove(from: from, to: to)
-        && pawnAvailableMove(of: pawn, position: to)
-    }
-    
-    // 현재 위치에 해당 색상의 pawn이 있는지 확인
-    func pawnAvailableCurrent(of pawn: Pawn, position: Position) -> Bool {
-        guard let currentPawn = board[position.rank.rawValue][position.file.rawValue] else { return false }
-        return currentPawn == pawn
-    }
-    
-    // 목표 위치에 같은 색상 의 pawn이 있는지 확인한다
-    func pawnAvailableMove(of pawn: Pawn, position: Position) -> Bool {
-        guard let toPwan = board[position.rank.rawValue][position.file.rawValue]
-        else {
-            return true
-        }
-        return toPwan != pawn
-    }
+//    func checkEnableToMove(pawn: Pawn, from: Position, to: Position) -> Bool {
+//        return pawnAvailableCurrent(of: pawn, position: from)
+//        && pawn.checkEnableToMove(from: from, to: to)
+//        && pawnAvailableMove(of: pawn, position: to)
+//    }
+//
+//    // 현재 위치에 해당 색상의 pawn이 있는지 확인
+//    func pawnAvailableCurrent(of pawn: Pawn, position: Position) -> Bool {
+//        guard let currentPawn = board[position.rank.rawValue][position.file.rawValue] else { return false }
+//        return true
+////        return currentPawn == pawn
+//    }
+//
+//    // 목표 위치에 같은 색상 의 pawn이 있는지 확인한다
+//    func pawnAvailableMove(of pawn: Pawn, position: Position) -> Bool {
+//        guard let toPwan = board[position.rank.rawValue][position.file.rawValue]
+//        else {
+//            return true
+//        }
+//        return toPwan != pawn
+//    }
 }
 
 extension Board {
+    func initBoard(with piece: Piece, position: [Position]) {
+        position.forEach {
+            board[$0.rank.rawValue][$0.file.rawValue] = piece
+        }
+    }
     
     // 현재 플레이중인 체스보드 표시
     func displayBoard() -> String{
@@ -71,7 +75,7 @@ extension Board {
             .enumerated()
             .forEach { index, files in
                 let ranks = files.map({ item -> String in
-                    return item?.color.display ?? "."
+                    return item?.display ?? "."
                 }).reduce("", { $0 + $1 })
                 display += "\(index + 1)"
                 display += ranks
@@ -82,14 +86,14 @@ extension Board {
     }
     
     // 현재 각 pawn의 남은 갯수를 알려줌
-    func score() -> String {
-        let avalilablePawn = board
-            .compactMap({ $0.compactMap({ $0 }) })
-            .flatMap({ $0 })
-        let validBlackPawn = avalilablePawn.filter( { $0 == Pawn(color: .black )})
-        let validWhitePawn = avalilablePawn.filter( { $0 == Pawn(color: .white )})
-        
-        let score = "현재 스코어\nblack -> \(Constants.maxPawnCount - validWhitePawn.count)\nwhite -> \(Constants.maxPawnCount - validBlackPawn.count)"
-        return score
-    }
+//    func score() -> String {
+//        let avalilablePawn = board
+//            .compactMap({ $0.compactMap({ $0 }) })
+//            .flatMap({ $0 })
+//        let validBlackPawn = avalilablePawn.filter( { $0 == Pawn(color: .black )})
+//        let validWhitePawn = avalilablePawn.filter( { $0 == Pawn(color: .white )})
+//
+//        let score = "현재 스코어\nblack -> \(Constants.maxPawnCount - validWhitePawn.count)\nwhite -> \(Constants.maxPawnCount - validBlackPawn.count)"
+//        return score
+//    }
 }
