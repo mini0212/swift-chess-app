b8b80436e10e580ff6e1677594e41673d4e2e4c8
Seongmin Kim <ksmini0212@gmail.com>
2022-10-06 21:45:27 +0900

    Position, King, Piece, Board 생셩.

--- SwiftChess/SwiftChess/main.swift
+++ SwiftChess/SwiftChess/main.swift
@@ -7,3 +7,31 @@
 
 import Foundation
 
+struct Position {
+    var rank: Int
+    var file: Int
+}
+
+enum Color {
+    case black
+    case white
+}
+
+protocol Piece {
+    var color: Color { get }
+    var position: Position { get }
+}
+
+struct King: Piece {
+    let color: Color
+    var position: Position
+}
+
+struct Board {
+    var data: [[Piece?]]
+    
+    init() {
+        let rank = [Piece?](repeating: nil, count: 8)
+        data = [[Piece?]](repeating: rank, count: 8)
+    }
+}
