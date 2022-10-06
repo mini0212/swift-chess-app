989c506d3e9e764a4e26a58bfba3e67af2be5631
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-06 21:35:15 +0900

    프로젝트 생성 및 테스트스킴 추가

---
+++ SwiftChess/SwiftChess/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  SwiftChess
+//
+//  Created by Sunghyun Kim on 2022/10/06.
+//
+
+import Foundation
+
---
+++ SwiftChess/SwiftChessTests/SwiftChessTests.swift
@@ -0,0 +1,31 @@
+//
+//  SwiftChessTests.swift
+//  SwiftChessTests
+//
+//  Created by Sunghyun Kim on 2022/10/06.
+//
+
+import XCTest
+
+final class SwiftChessTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
